94240cb77520bd0fccfd1f4240d7a7c8c4057ae2
Piyush Malaviya <piyushmalaviya1991@gmail.com>
2023-04-08 16:35:32 +0530

    Apply changs from Linux

--- EP002/source/main.cpp
+++ EP002/source/main.cpp
@@ -8,9 +8,15 @@ T add(T a, T b)
     return a+b;
 }
 
-int main()
+int main(int argc, char** argv)
 {
+    if(argc != 3)
+    {
+	std::cerr << "Command line arrgument should be 2" << std::endl;
+	return -1;
+    }
+
     std::cout<<"Hellow World from c++20 with CMake on Linux/Windows"<<std::endl;
-    std::cout<<"The Sum is: " << add(7, 5) << std::endl;
+    std::cout<<"The Sum is: " << add(atoi(argv[1]), atoi(argv[2])) << std::endl;
     return 0;
 }
