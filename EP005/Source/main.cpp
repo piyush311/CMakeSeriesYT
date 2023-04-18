bfd6aae0c6e9831a109b2be98415eaaf471ab2fe
Piyush Malaviya <piyushmalaviya1991@gmail.com>
2023-04-18 19:49:35 +0530

    Add code for episode 5

---
+++ EP005/Source/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "dog.h"
+#include "Operation.h"
+
+int main(int argc, char** argv)
+{
+    Dog dog1("flitzy");
+    dog1.print_info();
+    
+     return 0;
+}
---
+++ EP005/Source/src/Operation.cpp
@@ -0,0 +1,6 @@
+#include "Operation.h"
+
+double add(double a, double b)
+{
+    return a + b;
+}
